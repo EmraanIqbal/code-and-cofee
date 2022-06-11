56a68115987d2cc73ff6f2b86d050566c44bb7b0
EmraanIqbal <se.imraniqbal@gmail.com>
2022-06-11 17:29:25 +0500

    Mongodb connected

--- app.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require("events");
-
-const emitter = new EventEmitter();
-
-emitter.on("logged", (arg) => {
-  console.log("Event Triggered", arg);
-});
-
-emitter.emit("logged", { age: 20, school: "gcul" });
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// const bodyparser = require("body-parser");    its not used in new version of express
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDb = require("./config/config");
+
+const app = express();
+dotenv.config({ path: ".env" });
+const PORT = process.env.PORT || 5000;
+
+connectDb();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("Hello EMi How are you");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`);
+});
