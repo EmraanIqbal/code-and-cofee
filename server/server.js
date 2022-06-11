e3ef7c551a86506b18993f929112b2124d4337df
EmraanIqbal <se.imraniqbal@gmail.com>
2022-06-11 17:39:59 +0500

    issue fixed

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", (req, res) => {
-  res.send("Hello Emi How are you");
+  res.send("Hello EMi How are you");
 });
 
 app.listen(PORT, () => {
